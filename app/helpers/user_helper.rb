815edc62e634532c5f706159165f902bc04551d6
Miguel Tucu Gomez <miguelgomez66@gmail.com>
2021-04-06 17:38:41 -0300

    Style user's image element

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -1,9 +1,23 @@
 module UserHelper
 
+  def gravatar_for(user, size: 60)
+    gravatar_id = Digest::MD5.hexdigest(user.email)
+    user.update(gravatar_url: "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}") if user.gravatar_url.nil?
+    image_tag(user.gravatar_url, alt: user.name, class: 'gravatar')
+  end
+
   def create_friendship(friend_id)
     user = User.find_by('id = ?', current_user.id)
     friend = User.find_by('id = ?', friend_id)
     Friendship.create(user: user, friend: friend) unless friend_id.nil?
+  end
+
+  def accept(user)
+    users_path(user, friend_id: user.id, confirm: true)
+  end
+
+  def reject(user)
+    users_path(user, friend_id: user.id, confirm: false)
   end
 
   def who_to_add(user)
