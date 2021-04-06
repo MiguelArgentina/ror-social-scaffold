f0bd1d7b62094c985ac18b79fe5594a7f73bcc69
George <71434803+george-swift@users.noreply.github.com>
2021-04-06 11:38:59 +0100

    Change method call for gravatar helper function

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -1,6 +1,6 @@
 module UserHelper
   def gravatar_for(user, size: 60)
-    gravatar_id = Digest::MD5::hexdigest(user.email)
+    gravatar_id = Digest::MD5.hexdigest(user.email)
     user.update(gravatar_url: "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}") if user.gravatar_url.nil?
     image_tag(user.gravatar_url, alt: user.name, class: 'gravatar')
   end
